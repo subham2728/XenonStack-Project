7b4baccdefe6cfddc61f1b9006ceba02ac365dbb
Subham <subham.rouniyar27@gmail.com>
2022-07-26 22:23:33 +0530

    added domain name

--- hello/settings.py
+++ hello/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)l&%0ppoy88l8!el9psxw4g8d+zr_v_av65pm%rj)fph_e&9=k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1","xenon-stack-project.herokuapp.com/"]
 
 
 # Application definition
